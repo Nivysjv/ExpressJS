8c204b7fd230b9f0a0c4019c696b90346dcb209c
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-05-09 15:04:58 -0300

    mensgem legau

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-/* GET users listing. */
-router.get('/joedio', function(req, res, next) {
-  res.send('Olá, Joédio!');
+// Rota principal para renderizar nossa lista
+router.get('/', (req, res) => {
+  // Dados dinâmicos (nosso vetor de elementos)
+  const meusUsuarios = [
+    { id: 1, nome: 'Roger', descricao: 'Descrição detalhada do Item Roger.' },
+    { id: 2, nome: 'Carlos', descricao: 'O segundo item da nossa lista Carlos.' },
+    { id: 3, nome: 'Heathcliff', descricao: 'Culpado por 2 Homicídios culposos' },
+    { id: 4, nome: 'gabrielUltrakill', descricao: 'Morreu na contra mão.' }
+  ];
+
+  // Renderiza a view 'usuarios.ejs' passando os dados
+  res.render('usuarios', {
+    titulo: 'Meus Usuários',
+    usuariosParaView: meusUsuarios // Passando o array para a view
+  });
 });
 
 module.exports = router;
